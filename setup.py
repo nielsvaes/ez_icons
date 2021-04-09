e24e934d299aa1423ba7892d6a9f8f750569138f
nielsvaes-es <niels.vaes@embark-studios.com>
2021-04-09 21:22:34 +0200

    bump to autobuild

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ data_files_to_include = ["*.png"]
 
 setup(
     name='ez-icons',
-    version='1.0.0',
+    version='1.0.1',
     packages=find_packages(),
     package_data={
         "": data_files_to_include,
